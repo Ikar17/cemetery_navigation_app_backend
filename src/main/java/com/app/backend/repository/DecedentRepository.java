f4defd1d580c47ce732edeadaef06e2bcebde43d
brzezin <236700@edu.p.lodz.pl>
2024-04-14 16:23:30 +0200

    Created Decedent model with repository

---
+++ src/main/java/com/app/backend/repository/DecedentRepository.java
@@ -0,0 +1,13 @@
+package com.app.backend.repository;
+
+import com.app.backend.model.Decedent;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DecedentRepository extends JpaRepository<Decedent, Integer> {
+    List<Decedent> findBySurname(String surname);
+
+}
